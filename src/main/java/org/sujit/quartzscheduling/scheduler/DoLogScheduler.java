683b16c3e115b63ee67675aa64e5c7d9d6d744b9
= <=>
2024-06-13 13:31:08 +0545

    configre logger scheduler

--- src/main/java/org/sujit/quartzscheduling/scheduler/DoLogScheduler.java
+++ src/main/java/org/sujit/quartzscheduling/scheduler/DoLogScheduler.java
@@ -1,11 +1,16 @@
 package org.sujit.quartzscheduling.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.sujit.quartzscheduling.constant.SchedulerConstant;
+import org.sujit.quartzscheduling.quartz.SingleTriggerCornJobScheduler;
+
+import java.util.TimeZone;
 
 @Component
 @Slf4j
@@ -16,5 +21,27 @@ public class DoLogScheduler  implements Job {
     public void execute(JobExecutionContext context) throws JobExecutionException {
         log.info("Logger Job get executed");
         log.info("Do log related task here !!");
+    }
+
+    @Configuration
+    class DoLogSchedulerJobConfig {
+        private final SingleTriggerCornJobScheduler singleTriggerCornJobScheduler;
+        private final String logSchedulerCornExpression;
+
+        public DoLogSchedulerJobConfig(SingleTriggerCornJobScheduler singleTriggerCornJobScheduler,
+                                       @Value("${quartz.logScheduler.expression: 0 * * * * *}") String logSchedulerCornExpression
+                                       ) {
+            this.singleTriggerCornJobScheduler = singleTriggerCornJobScheduler;
+            this.logSchedulerCornExpression = logSchedulerCornExpression;
+        }
+
+
+        @EventListener(ApplicationReadyEvent.class)
+        public void scheduleLogSchedulerJob() throws SchedulerException {
+            singleTriggerCornJobScheduler.scheduleJob(JobKey.jobKey("DoLogScheduler"),
+                    DoLogScheduler.class,
+                    logSchedulerCornExpression,
+                    SchedulerConstant.DEFAULT_TIMEZONE);
+        }
     }
 }
