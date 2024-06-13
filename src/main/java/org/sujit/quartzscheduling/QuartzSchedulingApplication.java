2102bad6891c55510ee0a6875d90bdff3bb3cdd9
= <=>
2024-06-13 12:12:08 +0545

    Initial Setup

---
+++ src/main/java/org/sujit/quartzscheduling/QuartzSchedulingApplication.java
@@ -0,0 +1,13 @@
+package org.sujit.quartzscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzSchedulingApplication.class, args);
+    }
+
+}
