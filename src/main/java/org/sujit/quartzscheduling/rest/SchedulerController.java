eca6619cdc61110fe76bd8ce462716a01b1321e3
sujit.sharma <sujit.sharma@foneloan.com>
2024-09-06 11:21:21 +0545

    chore: update scheduler from rest api

--- src/main/java/org/sujit/quartzscheduling/rest/SchedulerController.java
+++ src/main/java/org/sujit/quartzscheduling/rest/SchedulerController.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.sujit.quartzscheduling.entity.JobDetails;
 import org.sujit.quartzscheduling.scheduler.PrintTimeSchedulerJobConfig;
+import org.sujit.quartzscheduling.service.JobDetailService;
+
+import static org.sujit.quartzscheduling.constant.JobConstant.PRINT_TIME_JOB_KEY;
 
 @RestController
 @RequestMapping("scheduler")
@@ -14,11 +18,18 @@ public class SchedulerController {
 
     private final PrintTimeSchedulerJobConfig printTimeSchedulerJobConfig;
 
+    private final JobDetailService jobDetailService;
+
     @PostMapping("/update")
     public ResponseEntity updateScheduler(@RequestBody SchedulerExpressionDto expressionDto) {
         printTimeSchedulerJobConfig.setPrintTimeSchedulerCornExpression(expressionDto.getExpression());
-
         printTimeSchedulerJobConfig.schedulePrintTimeSchedulerJob();
+
+        JobDetails jobDetails = new JobDetails();
+        jobDetails.setJobKey(PRINT_TIME_JOB_KEY);
+        jobDetails.setExpression(expressionDto.getExpression());
+
+        jobDetailService.saveOrUpdateJobDetail(jobDetails);
 
         return ResponseEntity.ok().build();
     }
